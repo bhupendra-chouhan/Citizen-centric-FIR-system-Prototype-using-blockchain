baa67381216ff81ee8e6c605e6432993bee5e5e2
Bhupendrachouhan19 <iiitianbhupendra@gmail.com>
2023-10-20 02:51:46 +0530

    Minor Updates

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <div className="">
       <Head>
-        <title>Complaint App</title>
+        <title>FIR System Prototype</title>
         <meta name="description" content="This is a police complaint app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
